798715d68595b68cc33ba945b532ecba0babc968
Nilank <sharmanilank.com>
2018-04-24 16:41:56 -0400

    Added Payment Component

---
+++ client/src/app/payment/payment.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserAccountService } from '../services/userAccount.service';
+import { OrderService } from '../services/order.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.css']
+})
+export class PaymentComponent implements OnInit {
+  myForm: FormGroup;
+
+  constructor(private userAccountService: UserAccountService, private orderService: OrderService, private router: Router) { }
+
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      holderName: new FormControl(null, Validators.required),
+      cardNumber: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('[0-9]{16}')
+      ]),
+      cvv: new FormControl(null, [
+        Validators.required
+      ])
+    });
+  }
+  }
+
+  onSubmit() {
+    this.orderService.placeOrder(this.orderService.order).subscribe(
+      data => {
+        console.log(data);
+        alert('Order Placed!!');
+        this.userAccountService.getCurrentUser()
+          .subscribe(() => {
+            window.location.replace('/home');
+            this.router.navigateByUrl('/');
+          });
+      },
+      error => console.error(error)
+    );
+    this.myForm.reset();
+  }
+}
